d3d3124be2f5f78a01b1190588294aa88be3863d
ASG40K <anthonygalindo185@gmail.com>
2025-05-09 23:16:29 -0700

    Database Create

--- NewDatabaseModel/AppDbContext.cs
+++ NewDatabaseModel/AppDbContext.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace NewDatabaseModel
-{
+namespace NewDatabaseModel;
     public partial class AppDbContext : DbContext
     {
+
+        public AppDbContext()
+        {
+        }
+
         public AppDbContext(DbContextOptions<AppDbContext> options)
             : base(options)
         {
@@ -15,12 +19,11 @@ namespace NewDatabaseModel
         public DbSet<CarListing> CarListings { get; set; } = null!;
 
 
-        // Add other DbSets as needed
+    // Add other DbSets as needed
 
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            base.OnModelCreating(modelBuilder);
-            // Optional: Fluent API configs here
-        }
-    }
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+  => optionsBuilder.UseSqlServer("server=(localdb)\\mssqllocaldb;database=CarMarketplace");
+
 }
+
---
+++ NewDatabaseModel/Migrations/20250510061047_InitialCreate.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace NewDatabaseModel.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CarListing",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    Make = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Model = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false),
+                    Description = table.Column<string>(type: "varchar(max)", unicode: false, nullable: true),
+                    UserID = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CarListing", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CarListing");
+        }
+    }
+}
